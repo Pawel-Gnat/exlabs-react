edd5ffd3821bd68dd5aea9d3eeb511baf210c55c
Pawel Gnat <pawel_gnat@o2.pl>
2024-08-15 08:38:35 +0200

    fix: app character context

--- src/components/homepage/GalleryListItem.tsx
+++ src/components/homepage/GalleryListItem.tsx
@@ -1,4 +1,5 @@
 import { Character } from '@/models/Character';
+import { Link } from 'react-router-dom';
 
 interface GalleryListItemProps {
   character: Character;
@@ -7,7 +8,7 @@ interface GalleryListItemProps {
 export const GalleryListItem = ({ character }: GalleryListItemProps) => {
   return (
     <li className="relative mx-auto w-fit overflow-hidden rounded-xl shadow-2xl">
-      <a className="group block" href={`/details/${character.id}`}>
+      <Link className="group block" to={`/details/${character.id}`}>
         <img
           alt={character.name}
           className="transition-transform duration-300 group-hover:scale-105 group-focus:scale-105"
@@ -16,7 +17,7 @@ export const GalleryListItem = ({ character }: GalleryListItemProps) => {
         <p className="absolute bottom-2 left-1/2 w-max -translate-x-1/2 rounded-xl border-2 bg-white px-4 py-2 font-bold transition-opacity duration-300 group-hover:opacity-60 group-focus:opacity-60">
           {character.name}
         </p>
-      </a>
+      </Link>
     </li>
   );
 };
